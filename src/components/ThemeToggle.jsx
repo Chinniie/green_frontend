2e6d46d227f8caac1918a77fb78699bde54efa71
Chinniie <chinruimy9@gmail.com>
2026-04-19 00:05:06 +0700

    Add files via upload

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from "react";
+import { Sun, Moon } from "lucide-react";
+
+export default function ThemeToggle() {
+  const [isDark, setIsDark] = useState(
+    localStorage.getItem("theme") === "dark",
+  );
+
+  useEffect(() => {
+    const root = document.documentElement;
+
+    if (isDark) {
+      root.classList.add("dark");
+      localStorage.setItem("theme", "dark");
+    } else {
+      root.classList.remove("dark");
+      localStorage.setItem("theme", "light");
+    }
+  }, [isDark]);
+
+  return (
+    <button onClick={() => setIsDark(!isDark)}>
+      {isDark ? <Sun size={20} /> : <Moon size={20} />}
+    </button>
+  );
+}
