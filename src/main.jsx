3c22a115b59e643bb56e406aa4c9d5c0718e9f33
Chinniie <chinruimy9@gmail.com>
2026-04-19 00:07:05 +0700

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,16 @@ import App from "./App";
 import "./index.css";
 import "./i18n"; // ✅ import i18n
 
+const saved = localStorage.getItem("theme");
+
+if (saved === "dark") {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
